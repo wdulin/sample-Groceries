5ffca5aaa03d60d526dfc5961ac186d8c46c54b5
vakrilov <alexander.vakrilov@telerik.com>
2016-08-17 14:23:30 +0300

    Testing with mocha

--- app/tests/shared/user/user.spec.ts
+++ app/tests/shared/user/user.spec.ts
@@ -1,31 +1,32 @@
-import "reflect-metadata";
+//make sure you import mocha-config before @angular/core
+import { assert } from "../../test-config";
 import { User } from "../../../shared";
 
-declare var describe: any;
-declare var expect: any;
-declare var it: any;
+// declare var describe: any;
+// declare var expect: any;
+// declare var it: any;
 
 describe("Email validation", function() {
   let user = new User();
 
   it("Should reject an empty email address", function () {
     user.email = "";
-    expect(user.isValidEmail()).toBe(false);
+    assert.isFalse(user.isValidEmail());
   });
 
   it("Should reject a malformed email addresses", function() {
     user.email = "nativescript";
-    expect(user.isValidEmail()).toBe(false);
+    assert.isFalse(user.isValidEmail());
 
     user.email = "nativescript@";
-    expect(user.isValidEmail()).toBe(false);
+    assert.isFalse(user.isValidEmail());
 
     user.email = "nativescript@isawesome";
-    expect(user.isValidEmail()).toBe(false);
+    assert.isFalse(user.isValidEmail());
   });
 
   it("Should accept valid email addresses", function() {
     user.email = "nativescript@isawesome.com";
-    expect(user.isValidEmail()).toBe(true);
+    assert.isTrue(user.isValidEmail());
   });
 });
